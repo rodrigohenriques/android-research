30cd712b479ff89007781289b0ae313c9abceda7
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-09-19 14:38:29 -0300

    adding dependency injection and renaming resources in login activity

--- app/src/main/java/com/github/android/research/application/ui/activities/LoginActivity.java
+++ app/src/main/java/com/github/android/research/application/ui/activities/LoginActivity.java
@@ -1,39 +1,72 @@
 package com.github.android.research.application.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.EditText;
 
 import com.github.android.research.R;
+import com.github.android.research.application.service.ApplicationService;
+import com.github.android.research.application.service.ApplicationServiceCallback;
+import com.github.android.research.application.service.ApplicationServiceError;
+import com.github.android.research.application.service.login.LoginInput;
+import com.github.android.research.application.service.login.LoginOutput;
+import com.github.android.research.infrastructure.modules.ApplicationModule;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.OnClick;
+
+public class LoginActivity extends BaseActivity implements ApplicationServiceCallback<LoginOutput> {
+    @Bind(R.id.login_edittext_username)
+    EditText mEditTextUsername;
+    @Bind(R.id.login_edittext_password)
+    EditText mEditTextPassword;
+
+    @Inject
+    @Named(ApplicationModule.LOGIN_SERVICE)
+    ApplicationService<LoginInput, LoginOutput> loginService;
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_login;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+    }
+
+    @OnClick(R.id.login_button)
+    public void performLogin() {
+        String username = mEditTextUsername.getText().toString();
+        String password = mEditTextPassword.getText().toString();
+
+        LoginInput input = new LoginInput(username, password);
+
+        loginService.execute(input, this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
+    public void onSuccess(LoginOutput loginOutput) {
+        final Snackbar snackbar = Snackbar.make(mEditTextUsername,
+                "Thre is " + loginOutput.researchesCount() + " researches available",
+                Snackbar.LENGTH_LONG);
+
+        snackbar.setAction("Ok", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        });
+
+        snackbar.show();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onError(ApplicationServiceError error) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
