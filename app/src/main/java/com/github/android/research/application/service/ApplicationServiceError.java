29c5d34b04986fec5f79cdd10f4d132984d9afe4
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-09-19 14:35:19 -0300

    adding application service error

---
+++ app/src/main/java/com/github/android/research/application/service/ApplicationServiceError.java
@@ -0,0 +1,25 @@
+package com.github.android.research.application.service;
+
+public class ApplicationServiceError {
+    private String code;
+    private String message;
+    private Exception e;
+
+    public ApplicationServiceError(String code, String message, Exception e) {
+        this.code = code;
+        this.message = message;
+        this.e = e;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Exception getE() {
+        return e;
+    }
+}
